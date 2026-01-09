452762bc8ffd999562fc5b34209280b2f11e3e42
damaske <damaske.vzk@gmail.com>
2026-01-09 22:29:44 +0400

    six

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -6,6 +6,8 @@ This module selects every 60th row of these columns
 
 
 def slice(df):
-    
+    '''
+    This fumction does same thing like above
+    '''
     df_somecolumns = df.loc[::60, ["High", "Low", "Close", "Volume_(BTC)"]]
     return df_somecolumns
