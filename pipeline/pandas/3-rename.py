41fa53eabb1751014fe2cd8cf1af852a690d0898
damaske <damaske.vzk@gmail.com>
2026-01-09 19:05:52 +0400

    five

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,5 +7,6 @@ def rename(df):
     This function renames the columns of a DataFrame
     '''
     df = df.rename(columns={"Timestamp" : "Datetime"},)
+    df["Datetime"] = pd.to_datetime(df["Datetime"], unit='s')
     updated_df = df[["Datetime", "Close"]]
     return updated_df
