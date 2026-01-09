1009d548ca974b245feb287a229220df6c210b16
damaske <damaske.vzk@gmail.com>
2026-01-09 21:56:35 +0400

    five

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -12,6 +12,6 @@ def array(df):
     '''
     This fumction does same thing like above
     '''
-    df_somecolums = df.loc[:, ["High", "Close"]].tail(10)
+    df_somecolums = df[["High", "Close"]].tail(10)
     arr = df_somecolums.to_numpy()
     return arr
