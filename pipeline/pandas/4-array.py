02d2428e24341cd4174a7a9200f59fdf2bc14c55
damaske <damaske.vzk@gmail.com>
2026-01-09 21:47:11 +0400

    five

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+This module selects last 10 rows and
+converts them into array
+'''
+
+
+import pandas as pd
+
+def array(df):
+    '''
+    This fumction does same thing like above
+    '''
+    df.loc[:, ["High", "Close"]].tail(10)
+    arr = df.to_numpy()
+    return arr
+    
