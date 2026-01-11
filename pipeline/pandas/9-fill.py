c40c509ffacf2544185b8ae46c68b1f1c4803a7e
damaske <damaske.vzk@gmail.com>
2026-01-11 15:37:46 +0400

    ten

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+This module fills data
+'''
+
+
+def fill(df):
+    '''
+    This fumction does same thing like above
+    '''
+    df = df.drop('Weighted_Price', axis=1)
+    df['Close'].fillna(method='ffill', inplace=True)
+    for col in ['High', 'Low', 'Open']:
+        df[col].fillna(df['Close'], inplace=True)
+    df['Volume_(BTC)', 'Volume_(Currency'].fillna(0, inplace=True)
+    return df
