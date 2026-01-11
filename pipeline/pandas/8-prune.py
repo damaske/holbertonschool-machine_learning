4ac4d004b413c93ea6f5946f4c75f00f4752dfb3
damaske <damaske.vzk@gmail.com>
2026-01-11 15:14:38 +0400

    nine

---
+++ pipeline/pandas/8-prune.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+This module drop NaNs
+'''
+
+
+def prune(df):
+    '''
+    This fumction does same thing like above
+    '''
+    df.dropna(subset=["Close"])
+    return df
