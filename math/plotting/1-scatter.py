036978b9cc907d9a8abbd657608478fbc7b8e018
damaske <damaske.vzk@gmail.com>
2026-01-11 23:26:06 +0400

    second

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+'''
+This module plots and labels data with title
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def scatter():
-
+    '''
+    This function does same thing as above
+    '''
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
