cba15655acc8c1bc8678ec5f1e194f94c9fba956
damaske <damaske.vzk@gmail.com>
2026-01-13 11:52:07 +0400

    Two is better than one

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -23,6 +23,6 @@ def two():
     plt.title("Exponential Decay of Radioactive Elements")
     plt.xlim(0, 20000)
     plt.ylim(0, 1)
-    plt.plot(x, y1, 'r--', label='C-14')
-    plt.plot(x, y2, 'g', label='Ra-226')
+    plt.plot(x, y1, 'r:', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
     plt.show()
