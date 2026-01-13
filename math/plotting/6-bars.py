4de3a9621d9ff9a06f154ef811d3e452d6cb1f7b
damaske <damaske.vzk@gmail.com>
2026-01-14 01:52:05 +0400

    some

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+Docstring for math.plotting.6-bars
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
+    """
+    Create a stacked bar chart showing quantities of different fruits per person.
+    """
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4,3))
     plt.figure(figsize=(6.4, 4.8))
