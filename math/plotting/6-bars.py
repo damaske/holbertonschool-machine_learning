943991384d3f662dcc1774265316f40e95ef598f
damaske <damaske.vzk@gmail.com>
2026-01-14 01:54:07 +0400

    some

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -11,32 +11,22 @@ def bars():
     Create a stacked bar chart showing quantities of different fruits per person.
     """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
-    
-    people = ['Farrah', 'Fred', 'Felicia']
-    x = np.arange(len(people))
-    width = 0.5
-
-    apples = fruit[0]
-    bananas = fruit[1]
-    oranges = fruit[2]
-    peaches = fruit[3]
-
-    plt.bar(x, apples, width=width, color='red', label='Apples')
-    plt.bar(x, bananas, width=width, bottom=apples,
-            color='yellow', label='Bananas')
-    plt.bar(x, oranges, width=width, bottom=apples + bananas,
-            color='#ff8000', label='Oranges')
-    plt.bar(x, peaches, width=width,
-            bottom=apples + bananas + oranges,
-            color='#ffe5b4', label='Peaches')
-
-    plt.xticks(x, people)
-    plt.ylabel('Quantity of Fruit')
-    plt.ylim(0, 80)
+    names = ['Farrah', 'Fred', 'Felicia']
+    column1 = fruit[0, :]
+    column2 = fruit[1, :]
+    column3 = fruit[2, :]
+    column4 = fruit[3, :]
+    plt.bar(names, column1, label='apples', width=0.5, color='red')
+    plt.bar(names, column2, label='bananas', bottom=column1, width=0.5,
+            color='yellow')
+    plt.bar(names, column3, label='oranges', bottom=column1+column2,
+            width=0.5, color='#ff8000')
+    plt.bar(names, column4, label='peaches', bottom=column1+column2+column3,
+            width=0.5, color='#ffe5b4')
     plt.yticks(np.arange(0, 81, 10))
-    plt.title('Number of Fruit per Person')
     plt.legend()
-
+    plt.title('Number of Fruit per Person')
+    plt.ylabel('Quantity of Fruit')
     plt.show()
