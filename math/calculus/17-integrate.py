e897eb357e545717d2c6cce5f801bda2029d0b75
damaske <damaske.vzk@gmail.com>
2026-01-18 17:55:12 +0400

    somesome

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -12,23 +12,23 @@ def poly_integral(poly, C=0):
      :param C: constant of integration
      """
      if not isinstance(poly, list):
-        return None
-    if not all(isinstance(c, (int, float)) for c in poly):
-        return None
-    if len(poly) == 0:
-        return [C] if C != 0 else None
-    if not isinstance(C, (int, float)):
-        return None
-
-    poly_int = [C]
-
-    for i, coef in enumerate(poly):
+         return None
+     if not all(isinstance(c, (int, float)) for c in poly):
+         return None
+     if len(poly) == 0:
+         return [C] if C != 0 else None
+     if not isinstance(C, (int, float)):
+         return None
+     
+     poly_int = [C]
+     
+     for i, coef in enumerate(poly):
         coeff = coef / (i + 1)
         if coeff.is_integer():
             coeff = int(coeff)
         poly_int.append(coeff)
-
-    while len(poly_int) > 1 and poly_int[-1] == 0:
-        poly_int.pop()
-
-    return poly_int
+        
+        while len(poly_int) > 1 and poly_int[-1] == 0:
+            poly_int.pop()
+            
+        return poly_int
