9ae7eac9f21bd8cfa59995bf0fd025c147d95ad4
damaske <damaske.vzk@gmail.com>
2026-01-18 16:40:15 +0400

    some

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+ calculates the derivative of a polynomial
+"""
+
+
+def poly_derivative(poly):
+    """
+    calculates the derivative of a polynomial
+    """ 
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(coef, (int, float)) for coef in poly):
+        return None
+    if len(poly) == 0:
+        return None
+    
+    der = [i * poly[i] for i in range(1, len(poly))]
+    if not any(der):
+        return [0]
+    return der
