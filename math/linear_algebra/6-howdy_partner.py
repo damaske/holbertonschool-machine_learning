03317beb89102fb61939ced65e0001ca14e2b4a4
damaske <damaske.vzk@gmail.com>
2026-01-20 21:46:11 +0000

    conc

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """concatenates two arrays"""
+    new_arr =[]
+    new_arr = arr1.append(arr2)
+    return new_arr
