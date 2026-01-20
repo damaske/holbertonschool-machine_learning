8cd3ca4f7629ccab75c28dd692aad067589d3a20
damaske <damaske.vzk@gmail.com>
2026-01-20 19:09:54 +0000

    length of matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,3 +3,10 @@
 def matrix_shape(matrix):
     """Calculates the shape of a matrix"""
     shape = []
+
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
