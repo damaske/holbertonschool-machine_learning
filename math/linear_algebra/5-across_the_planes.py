02bdc23609550447b64b2a830ab72036f317169c
damaske <damaske.vzk@gmail.com>
2026-01-20 20:19:46 +0000

    adding matrices elements

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,4 +3,22 @@
 
 def add_matrices2D(mat1, mat2):
     """function adds matrices elements"""
+    new_mat = []
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    for i in range(len(mat1)):
+        row1 = len(mat1)
+        row2 = len(mat2)
+
+        if len(row1) != len(row2):
+            return None
+        new_row = []
+
+        for j in range(len(row1)):
+            new_row.append(row1[j] + row2[j])
+
+        new_mat.append(new_row)
+
+    return new_mat
 
