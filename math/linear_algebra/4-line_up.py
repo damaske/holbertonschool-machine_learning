f00258d57b3195a3994ded748cc68dffba7aa164
damaske <damaske.vzk@gmail.com>
2026-01-20 19:54:22 +0000

    add two arr

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,4 +4,9 @@
 
 def add_arrays(arr1, arr2):
     """finction adds two arrays element-wise"""
-
+    new_arr = []
+    if len(arr1) != len(arr2):
+        return None
+    for i in range(len(arr1)):
+        new_arr.append(arr1[i] + arr2[i])
+    return new_arr
