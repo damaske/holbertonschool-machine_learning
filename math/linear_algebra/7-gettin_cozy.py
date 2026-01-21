b9bde5b139b533a3caa0b754ed63102eac99bb03
damaske <damaske.vzk@gmail.com>
2026-01-21 08:20:40 +0000

    adding row

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,4 +4,19 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """def cat_matrices2D(mat1, mat2, axis=0)"""
+    if axis != 0:
+        return None
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    new_mat = []
+
+    for row in mat1:
+        new_mat.append(row[:])
+
+    for row in mat2:
+        new_mat.append(row[:])
+
+    return new_mat
 
