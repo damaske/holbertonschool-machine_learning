d3cb630adc415f43a031beb51a36203f57a2e86f
damaske <damaske.vzk@gmail.com>
2026-01-21 12:17:27 +0000

    some

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,4 +4,16 @@
 
 def mat_mul(mat1, mat2):
     """function matrix multiplication"""
-
+    if len(mat1[0]) != len(mat2):
+        return None
+    else:
+        new = []
+        for row1 in range(len(mat1)):
+            inner = []
+            for col2 in range(len(mat2[0])):
+                number = 0
+                for col1 in range(len(mat1[0])):
+                    number += (mat1[row1][col1] * mat2[col1][col2])
+                inner.append(number)
+            new.append(inner)
+        return new
