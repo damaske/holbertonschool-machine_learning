24c161da90b7452f83c42dfc75ffd8cf99bb7497
damaske <damaske.vzk@gmail.com>
2026-01-21 22:14:12 +0400

    some

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""code that multiplies two matrices"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """function that multiplies two matrices"""
+    return np.matmul(mat1, mat2)
