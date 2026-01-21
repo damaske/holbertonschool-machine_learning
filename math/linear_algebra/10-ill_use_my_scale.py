22d81b2d8bbb631f62c3843a52b1d42f1e82029d
damaske <damaske.vzk@gmail.com>
2026-01-21 21:13:45 +0400

    some shape

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""code that  that calculates the shape of a numpy.ndarray"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """calculates the shape of a numpy.ndarray"""
+    return matrix.shape
