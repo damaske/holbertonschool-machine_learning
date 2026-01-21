b7427e1f8560499b38e87b3c8998d814c868f000
damaske <damaske.vzk@gmail.com>
2026-01-21 22:10:16 +0400

    some

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""code that preforms aritmetic operations to matrices"""
+
+
+def np_elementwise(mat1, mat2):
+    """aritmetic operations to matrices"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
