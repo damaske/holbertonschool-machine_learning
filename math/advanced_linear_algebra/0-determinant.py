61742165be10f6f582038a68c02c65e3a7851340
damaske <damaske.vzk@gmail.com>
2026-02-10 23:05:47 +0400

    calculate the determinant of a matrix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 """code that calculates the determinant of a matrix"""
-import numpy as np
 
 
 def determinant(matrix):
     """function that calculates the determinant of a matrix"""
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.shape[0] != matrix.shape[1]:
-        raise ValueError("matrix must be square")
-    return np.linalg.det(matrix)
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    if not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+    
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0] 
+    det = 0
+    for i in range(len(matrix)):
+        sub_matrix = [row[:i] + row[i + 1:] for row in matrix[1:]]
+        det += ((-1) ** i) * matrix[0][i] * determinant(sub_matrix)
+    return det
