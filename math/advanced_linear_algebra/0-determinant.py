7f51bd74c89ed66944640698304b99f72a2e3c5b
damaske <damaske.vzk@gmail.com>
2026-02-10 23:08:29 +0400

    calculate the determinant of a matrix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -4,17 +4,18 @@
 
 def determinant(matrix):
     """function that calculates the determinant of a matrix"""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or len(matrix) == 0:
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         return 1
     if not all(len(row) == len(matrix) for row in matrix):
         raise ValueError("matrix must be a square matrix")
-    
+
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0] 
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det = 0
     for i in range(len(matrix)):
         sub_matrix = [row[:i] + row[i + 1:] for row in matrix[1:]]
