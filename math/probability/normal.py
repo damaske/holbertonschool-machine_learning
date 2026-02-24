66283d5ce1c80fe633e1a1223f85438bdfebc85f
damaske <damaske.vzk@gmail.com>
2026-02-25 02:24:05 +0400

    cdf

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -39,3 +39,23 @@ class Normal:
         ) * e ** (
             -0.5 * ((x - self.mean) / self.stddev) ** 2
         )
+
+    def cdf(self, x):
+        """Calculates the value of the CDF for a given x-value"""
+        e = 2.7182818285
+        pi = 3.1415926536
+        return 0.5 * (
+            1 + self.erf(
+                (x - self.mean) / (self.stddev * 2 ** 0.5)
+            )
+        )
+
+    def erf(self, x):
+        """Calculates the error function of a given x-value"""
+        e = 2.7182818285
+        pi = 3.1415926536
+        return (
+            2 / pi ** 0.5 * (
+                x - x ** 3 / 3 + x ** 5 / 10 - x ** 7 / 42 + x ** 9 / 216
+            )
+        )
