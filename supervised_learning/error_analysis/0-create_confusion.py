be4b500971ada932549adca179294986c1208f30
damaske <damaske.vzk@gmail.com>
2026-03-22 15:33:00 +0400

    confution matrix

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Creates a confusion matrix from the given labels and logits."""
+import numpy as np
+
+def create_confusion_matrix(labels, logits):
+    """Creates a confusion matrix from the given labels and logits."""
