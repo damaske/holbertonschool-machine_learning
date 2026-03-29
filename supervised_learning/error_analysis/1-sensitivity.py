6a790073309781c21739c8d21855b3bc0c68d486
damaske <damaske.vzk@gmail.com>
2026-03-30 02:31:13 +0400

    sensitivity

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -6,6 +6,8 @@ import numpy as np
 def sensitivity(confusion):
     """function Calculates the sensitivity"""
     TP = np.diag(confusion)
+    #находишь диагональ на одной страке которая есть 
     FN = np.sum(confusion, axis=1) - TP
+    #из суммы по строке вычитаешь TP и получаешь FN
     sensitivity = TP / (TP + FN)
     return sensitivity
