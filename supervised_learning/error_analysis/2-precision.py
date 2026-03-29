7bc19c7b28cc7b148ec4d2fd55543dec0f029994
damaske <damaske.vzk@gmail.com>
2026-03-30 03:22:39 +0400

    precision

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Calculates the sensitivity for each class in a confusion matrix."""
+import numpy as np
+
+
+def precision(confusion):
+    """function Calculates the precision"""
+    TP = np.diag(confusion)
+    #находишь диагональ на одной страке которая есть 
+    FP = np.sum(confusion, axis=0) - TP
+    #из суммы по столбцу вычитаешь TP и получаешь FP
+    precision = TP / (TP + FP)
+    return precision
