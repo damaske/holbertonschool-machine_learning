2712fcfef6bd43d9e04eeb2e058ca926779e6cac
damaske <damaske.vzk@gmail.com>
2026-03-30 19:42:45 +0400

    f1score

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -6,11 +6,11 @@ import numpy as np
 def f1_score(confusion):
     """function Calculates the F1 score"""
     TP = np.diag(confusion)
-    # находишь диагональ на одной страке которая есть
+    #находишь диагональ на одной страке которая есть
     FP = np.sum(confusion, axis=0) - TP
-    # из суммы по столбцу вычитаешь TP и получаешь FP
+    #из суммы по столбцу вычитаешь TP и получаешь FP
     FN = np.sum(confusion, axis=1) - TP
-    # из суммы по строке вычитаешь TP и получаешь FN
+    #из суммы по строке вычитаешь TP и получаешь FN
     precision = TP / (TP + FP)
     sensitivity = TP / (TP + FN)
     f1_score = 2 * (precision * sensitivity) / (precision + sensitivity)
