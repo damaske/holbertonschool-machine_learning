54e646508395eb67f7983509ec1112cfd6815a35
damaske <damaske.vzk@gmail.com>
2026-03-30 19:49:37 +0400

    f1score

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -3,12 +3,25 @@
 import numpy as np
 
 
+# def f1_score(confusion):
+#     """function Calculates the F1 score"""
+#     TP = np.diag(confusion)
+#     FP = np.sum(confusion, axis=0) - TP
+#     FN = np.sum(confusion, axis=1) - TP
+#     precision = TP / (TP + FP)
+#     sensitivity = TP / (TP + FN)
+#     f1_score = 2 * (precision * sensitivity) / (precision + sensitivity)
+#     return f1_score
+
+precision = __import__('2-precision').precision
+sensitivity = __import__('1-sensitivity').sensitivity
+
+
 def f1_score(confusion):
-    """function Calculates the F1 score"""
-    TP = np.diag(confusion)
-    FP = np.sum(confusion, axis=0) - TP
-    FN = np.sum(confusion, axis=1) - TP
-    precision = TP / (TP + FP)
-    sensitivity = TP / (TP + FN)
-    f1_score = 2 * (precision * sensitivity) / (precision + sensitivity)
-    return f1_score
+    '''
+    My function document
+    '''
+    p = precision(confusion)
+    s = sensitivity(confusion)
+
+    return 2 * (p * s) / (p + s)
