4da67e63a95fbf23566eb7d022771f96e2dea222
damaske <damaske.vzk@gmail.com>
2026-04-06 02:35:00 +0400

    printing tree

---
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""Build a decision tree."""
+import numpy as np
+
+
+class Node:
+    """Node class for a decision tree."""
+
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        """Initialize a node."""
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Return the maximum depth below this node."""
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
+    def count_nodes_below(self, only_leaves=False):
+        """Count the number of nodes below this node."""
+        left_count = self.left_child.count_nodes_below(
+            only_leaves=only_leaves
+        )
+        right_count = self.right_child.count_nodes_below(
+            only_leaves=only_leaves
+        )
+
+        if only_leaves:
+            return left_count + right_count
+        return 1 + left_count + right_count
+    
+    def left_child_add_prefix(self,text):
+    lines=text.split("\n")
+    new_text="    +--"+lines[0]+"\n"
+    for x in lines[1:] :
+        new_text+=("    |  "+x)+"\n"
+    return (new_text)
+
+    def left_child_add_prefix(text):
+    lines=text.split("\n")
+    new_text="    +--"+lines[0]+"\n"
+    for x in lines[1:] :
+        new_text+=("    |  "+x)+"\n"
+    return (new_text)
+
+
+
+class Leaf(Node):
+    """Leaf class for a decision tree."""
+
+    def __init__(self, value, depth=None):
+        """Initialize a leaf."""
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Return the depth of this leaf."""
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """Count the number of nodes below this leaf."""
+        return 1
+    
+    def __str__(self):
+        return (f"-> leaf [value={self.value}]")
+
+
+class Decision_Tree():
+    """Decision tree class."""
+
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """Initialize a decision tree."""
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Return the maximum depth of the tree."""
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        """Count the number of nodes in the tree."""
+        return self.root.count_nodes_below(only_leaves=only_leaves)
+    
+    def __str__(self):
+        return self.root.__str__()
