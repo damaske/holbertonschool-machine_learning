a72e43ab7c5badd89c1db743199966d9b70f3d36
damaske <damaske.vzk@gmail.com>
2026-04-07 15:14:38 +0400

    printing tree

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -40,23 +40,16 @@ class Node:
         """Add prefix to the left child string."""
         lines = text.split("\n")
         new_text = "+--" + lines[0] + "\n"
-
         for x in lines[1:]:
-            if x.startswith("+--") or x.startswith("->"):
-                new_text += "| " + x + "\n"
-            else:
-                new_text += x + "\n"
-
+            new_text += "| " + x + "\n"
         return new_text
 
     def right_child_add_prefix(self, text):
         """Add prefix to the right child string."""
         lines = text.split("\n")
         new_text = "+--" + lines[0] + "\n"
-
         for x in lines[1:]:
             new_text += x + "\n"
-
         return new_text
 
     def __str__(self):
