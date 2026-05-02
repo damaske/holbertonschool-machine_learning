0581fbe78df14e34fefed11d8720f46721b9abc9
damaske <damaske.vzk@gmail.com>
2026-05-02 19:08:35 +0400

    privatize

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Task 0: 0. Privatize Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """
+    A class used to represent a single neuron in a neural network.
+
+    Attributes
+   __ W : numpy.ndarray
+        The weight vector for the neuron
+    __ b : float
+        The bias for the neuron, initialized to 0.
+    __ A : float
+        The activation output of the neuron, initialized to 0.
+
+    Methods
+    __init__(self, nx)
+        Initializes a neuron with `nx` input features.
+    """
+    def __init__(self, nx):
+        """
+        Initialize the Neuron instance.
+
+        Parameters
+        nx : int
+            The number of input features to the neuron. It must be a
+            positive integer.
+
+        Raises
+        TypeError
+            If `nx` is not an integer.
+        ValueError
+            If `nx` is less than 1.
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        else:
+            self.__W = np.random.normal(0, 1, (1, nx))
+            self.__b = 0
+            self.__A = 0
+
+    @property
+    def W(self):
+        """ Getter for the weight vector W. """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter for the bias b. """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter for the activation output A. """
+        return self.__A
